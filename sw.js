eb48c0aafd1f20908c8bfef7463242e66d4019f9
andreagx <andrea.gallazzi@live.com>
2026-08-25 23:06:20 +0200

    Finalize mobile workout PWA and infographic sprite

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pale-v2';
+const CACHE='pale-v3';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./sprite.webp'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
