cc11bc3040f0c7b6684727ba54a1a55727447bfa
andreagx <andrea.gallazzi@live.com>
2026-08-26 08:06:22 +0200

    Refresh PWA cache for image layout fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='pale-v3';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./sprite.webp'];
+const CACHE='pale-v4';
+const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icon.svg','./sprite.webp'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
