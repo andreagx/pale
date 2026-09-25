ab1665dee3665b99a5c17499cc3f946c806d1411
andreagx <andrea.gallazzi@live.com>
2026-09-25 12:44:49 +0200

    Use network-first PWA assets and bump cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pale-v17';
+const CACHE='pale-v25';
 const ASSETS=['./','./index.html','./program.js','./app.js','./reset-all.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil((async()=>{
@@ -19,5 +19,5 @@ self.addEventListener('fetch',event=>{
       return await cache.match('./index.html')||Response.error();
     })());return;
   }
-  event.respondWith((async()=>{const cache=await caches.open(CACHE);return await cache.match(event.request)||fetch(event.request);})());
+  event.respondWith((async()=>{const cache=await caches.open(CACHE);try{const response=await fetch(event.request,{cache:'no-store'});if(response.ok){await cache.put(event.request,response.clone());return response;}}catch(_){} return await cache.match(event.request)||Response.error();})());
 });
